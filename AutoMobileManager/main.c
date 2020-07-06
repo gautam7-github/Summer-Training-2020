ac0d26d485af42164500ea268f38e60da108004e
Gautam <61959145+gautam7-github@users.noreply.github.com>
2020-07-06 12:57:00 +0530

    automobile management
    project made by : Gautam of MRSPTU

--- AutoMobileManager/main.c
+++ AutoMobileManager/main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+
+//functions
+void display_menu();
+int choices();
+void add();
+void full_displ();
+void delete();
+
 struct vehicle_details
 {
     char mnfctr[20];
     int mnfctr_year;
     char vname[20];
     char modelname[20];
-    int vehicle_nmbr;
+    unsigned int vehicle_nmbr;
     char buyer_name[30];
-    char buyer_contact[10];
+    int buyer_contact[10];
     char buyer_address[100];
 
 };
 int main()
 {
+    system("COLOR 2");
     display_menu();
     choices();
     return EXIT_SUCCESS;
@@ -32,11 +42,13 @@ void display_menu()
 }
 int choices()
 {
+
     int choice;
     printf("\n Enter 1 to Add Entry\t\t||\t");
-    printf(" Enter 2 to View Entrie(s) ");
+    printf(" Enter 2 to View Full Record ");
     printf("\n Enter 3 to Search Entry\t||\t");
-    printf(" Enter 4 to Delete All \n\n");
+    printf(" Enter 4 to Delete All \n");
+    printf(" Enter 5 to Exit Program    \t||\n\n");
     printf(" ENTER YOUR DESIRED CHOICE: ");
     scanf("%i",&choice);
 
@@ -53,12 +65,15 @@ int choices()
     case 4:
         delete();
         break;
+    case 5:
+        exit(EXIT_SUCCESS);
     default:
         printf("OOPS....WRONG CHOICE\n");
+        break;
         getch();
         system("cls");
-        main();
     }
+    return 0;
 }
 void add()
 {
@@ -74,6 +89,7 @@ void add()
 
     for(i=0;i<n;i++)
     {
+        printf("\t\tVEHICLE %d\n\n",i+1);
         printf("ENTER MANUFACTURER          : ");
         scanf("%s",vehicles[i].mnfctr);
 
@@ -87,31 +103,45 @@ void add()
         scanf("%s",vehicles[i].modelname);
 
         printf("ENTER VEHICLE NUMBER        : ");
-        scanf("%d",&vehicles[i].vehicle_nmbr);
+        scanf("%ud",&vehicles[i].vehicle_nmbr);
 
         printf("ENTER BUYER NAME            : ");
         scanf("%s",vehicles[i].buyer_name);
 
+        printf("ENTER BUYER CONTACT         : ");
+        for(int k=0;k<10;k++)
+        {
+            scanf("%i",&vehicles[i].buyer_contact[k]);
+        }
+
         printf("--------------------------------------------\n");
 
+        fprintf(fptr,"\tVEHICLE %i\n",i+1);
         fprintf(fptr,"MANUFACTURER          : %s  \n",vehicles[i].mnfctr);
         fprintf(fptr,"MANUFACTURING YEAR    : %i  \n",vehicles[i].mnfctr_year);
         fprintf(fptr,"VEHICLE NAME          : %s  \n",vehicles[i].vname);
         fprintf(fptr,"MODEL NAME            : %s  \n",vehicles[i].modelname);
-        fprintf(fptr,"VEHICLE NUMBER        : %i  \n",vehicles[i].vehicle_nmbr);
-        fprintf(fptr,"BUYER NAME            : %s  \n\n",vehicles[i].buyer_name);
+        fprintf(fptr,"VEHICLE NUMBER        : %ud \n",vehicles[i].vehicle_nmbr);
+        fprintf(fptr,"BUYER NAME            : %s  \n",vehicles[i].buyer_name);
+        fprintf(fptr,"BUYER CONTACT         : ");
+        for(int k=0;k<10;k++)
+           {
+               fprintf(fptr,"%d",vehicles[i].buyer_contact[k]);
+           }
+        fprintf(fptr,"\n\n");
+
     }
     printf("\n %i ENTRIE(S) ADDED.....",n);
 
     fclose(fptr);
+
     getch();
     system("cls");
-    display_menu();
-    choices();
+    main();
 }
 void full_displ()
 {
-    char reader;
+    char reader,str[60];
 
     FILE *fptr;
 
@@ -119,17 +149,29 @@ void full_displ()
 
     if (fptr == NULL)
     {
-        printf("NO ENTRIES AVAILABLE....");
+        printf(" FILE ERROR....");
+        Sleep(2000);
+        printf(" CONTACT IT DEPARTMENT....");
         exit(1);
     }
     else
     {
-        while(!feof(fptr))
+        if (fgets(str,60,fptr)==NULL)
         {
-            reader = fgetc(fptr);
-            printf("%c",reader);
+            printf(" NO ENTRIES AVAILABLE.....");
+            getch();
+            system("cls");
+            main();
         }
-    fclose(fptr);
+        else
+        {
+            while(!feof(fptr))
+            {
+                reader = fgetc(fptr);
+                printf("%c",reader);
+            }
+        fclose(fptr);
+        }
     }
     getch();
     system("cls");
